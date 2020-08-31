796d2dc6a9b62b2c47d88842d31278f5341d0342
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2020-08-31 18:02:27 +0300

    FIR IDE: introduce multi module lazy resolve tests

---
+++ idea/idea-frontend-fir/idea-fir-low-level-api/tests/org/jetbrains/kotlin/idea/fir/low/level/api/AbstractFirMultiModuleLazyResolveTest.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.idea.fir.low.level.api
+
+import com.intellij.openapi.util.io.FileUtil
+import com.intellij.openapi.vfs.VirtualFileManager
+import com.intellij.psi.PsiManager
+import com.intellij.workspace.virtualFileUrl
+import org.jetbrains.kotlin.fir.declarations.FirResolvePhase
+import org.jetbrains.kotlin.fir.render
+import org.jetbrains.kotlin.idea.stubs.AbstractMultiModuleTest
+import org.jetbrains.kotlin.idea.util.sourceRoots
+import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.kotlin.test.KotlinTestUtils
+import java.io.File
+import java.nio.file.Paths
+
+abstract class AbstractFirMultiModuleLazyResolveTest : AbstractMultiModuleTest() {
+    override fun getTestDataPath(): String =
+        "${KotlinTestUtils.getHomeDirectory()}/idea/idea-frontend-fir/idea-fir-low-level-api/testdata/multiModuleLazyResolve/"
+
+    fun doTest(path: String) {
+        val testStructure = TestProjectStructureReader.read(Paths.get(path))
+        val modulesByNames = testStructure.modules.associate { moduleData ->
+            moduleData.name to module(moduleData.name)
+        }
+        testStructure.modules.forEach { moduleData ->
+            val module = modulesByNames.getValue(moduleData.name)
+            moduleData.dependsOnModules.forEach { dependencyName ->
+                module.addDependency(modulesByNames.getValue(dependencyName))
+            }
+        }
+        val moduleToResolve = modulesByNames.getValue(testStructure.fileToResolve.moduleName)
+        val fileToAnalysePath = moduleToResolve.sourceRoots.first().virtualFileUrl.toString() + "/" + testStructure.fileToResolve.relativeFilePath
+
+        val virtualFileToAnalyse = VirtualFileManager.getInstance().findFileByUrl(fileToAnalysePath )
+            ?: error("File ${testStructure.fileToResolve.filePath} not found")
+        val ktFileToAnalyse = PsiManager.getInstance(project).findFile(virtualFileToAnalyse) as KtFile
+        val resolveState = LowLevelFirApiFacade.getResolveStateFor(ktFileToAnalyse)
+        val fir = LowLevelFirApiFacade.getOrBuildFirFor(ktFileToAnalyse, resolveState, FirResolvePhase.BODY_RESOLVE)
+        KotlinTestUtils.assertEqualsToFile(File("$path/expected.txt"), fir.render())
+    }
+}
